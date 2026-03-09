b7a890c33ae173df4b624395128f694ea35d7e89
Kevin <kevinstaebler1994@outlook.de>
2026-03-09 22:53:55 +0100

    project modal has been implemented and styled. JS projects-data has also been added

---
+++ js/projects-data.js
@@ -0,0 +1,23 @@
+const projectList = {
+  join: {
+    title: "Join",
+    description:
+      "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.",
+    languages: [
+      { name: "HTML", icon: "assets/icons/HTML.svg" },
+      { name: "CSS", icon: "assets/icons/CSS.svg" },
+      { name: "JavaScript", icon: "assets/icons/JavaScript.svg" },
+    ],
+  },
+
+  elPolloLoco: {
+    title: "El Pollo Loco",
+    description:
+      "Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen",
+    languages: [
+      { name: "HTML", icon: "assets/icons/HTML.svg" },
+      { name: "CSS", icon: "assets/icons/CSS.svg" },
+      { name: "JavaScript", icon: "assets/icons/JavaScript.svg" },
+    ],
+  },
+};
