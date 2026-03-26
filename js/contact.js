37476cfecca563da845dab983054b21a1b506312
Kevin <kevinstaebler1994@outlook.de>
2026-03-26 20:47:17 +0100

    added errorMessages object for validation purposes, still in progress

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,9 @@
 const form = document.getElementById("contactForm");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+const checkbox = document.getElementById("privacy");
+const submitBtn = document.getElementById("submitBtn");
 
 const pattern = {
   name: /^[a-zA-ZÀ-ž\s]{2,50}$/,
@@ -6,16 +11,28 @@ const pattern = {
   message: /^[\s\S]{10,500}$/,
 };
 
+const errorMessages = {
+  name: "Oops! It seems your name is missing",
+  email: "Hoppla! Your email is required",
+  message: "What do you need to develop?",
+};
+
+function isValid() {
+  return pattern.name.test(name.value) && pattern.email.test(email.value) && pattern.message.test(message.value) && checkbox.checked;
+}
+
 form.addEventListener("submit", function (event) {
   event.preventDefault();
+  if (!isValid()) return;
+  console.log("Mail gesendet");
 
-  emailjs.sendForm("service_urv966s", "template_6ro37zj", form).then(
-    function () {
-      alert("Nachricht erfolgreich gesendet!");
-      form.reset();
-    },
-    function (error) {
-      alert("Etwas ist schiefgelaufen: " + error.text);
-    }
-  );
+  // emailjs.sendForm("service_urv966s", "template_6ro37zj", form).then(
+  //   function () {
+  //     alert("Nachricht erfolgreich gesendet!");
+  //     form.reset();
+  //   },
+  //   function (error) {
+  //     alert("Etwas ist schiefgelaufen: " + error.text);
+  //   }
+  // );
 });
--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 function init() {
   initHoverImagePreview();
   renderReferences();
-  validateName();
 }
 
 document.addEventListener("DOMContentLoaded", init);
