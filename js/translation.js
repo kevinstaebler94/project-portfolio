7c83bf6105b19be8a41dcff57f178bd9a21a6b37
Kevin <kevinstaebler1994@outlook.de>
2026-04-06 15:45:27 +0200

    data attributes have been added to all mandatory fields

---
+++ js/translation.js
@@ -0,0 +1,38 @@
+function getTranslations() {
+  return {
+    de: getDe(),
+    en: getEn(),
+  };
+}
+
+function getDe() {
+  return {
+    de: {
+      header: {},
+      banner: {},
+      buttons: {},
+      about: {},
+      skills: {},
+      projects: {},
+      references: {},
+      contact: {},
+      footer: {},
+    },
+  };
+}
+
+function getEn() {
+  return {
+    en: {
+      header: {},
+      banner: {},
+      buttons: {},
+      about: {},
+      skills: {},
+      projects: {},
+      references: {},
+      contact: {},
+      footer: {},
+    },
+  };
+}
