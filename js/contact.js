29527b4e9d8236b262563f4163031f3324c2b525
Kevin <kevinstaebler1994@outlook.de>
2026-04-12 16:02:29 +0200

    animation and animation rule for form submit button has been implemented and structure has been changed

--- js/contact.js
+++ js/contact.js
@@ -24,74 +24,96 @@ const errorMessages = {
   message: "What do you need to develop?",
 };
 
-function nameValidation() {
-  if (!pattern.name.test(name.value)) {
-    name.classList.add("error");
-    name.placeholder = errorMessages.name;
-    name.value = "";
-    setTimeout(() => {
-      name.classList.remove("error");
-      name.placeholder = defaultPlaceholders.name;
-    }, 3000);
-    return false;
-  } else {
+function isNameValid() {
+  return pattern.name.test(name.value);
+}
+
+function showNameError() {
+  name.classList.add("error");
+  name.placeholder = errorMessages.name;
+  name.value = "";
+
+  setTimeout(() => {
     name.classList.remove("error");
     name.placeholder = defaultPlaceholders.name;
-    return true;
-  }
+  }, 3000);
 }
 
-function emailValidation() {
-  if (!pattern.email.test(email.value)) {
-    email.classList.add("error");
-    email.placeholder = errorMessages.email;
-    email.value = "";
-    setTimeout(() => {
-      email.classList.remove("error");
-      email.placeholder = defaultPlaceholders.email;
-    }, 3000);
-    return false;
-  } else {
+function isEmailValid() {
+  return pattern.email.test(email.value);
+}
+
+function showEmailError() {
+  email.classList.add("error");
+  email.placeholder = errorMessages.email;
+  email.value = "";
+
+  setTimeout(() => {
     email.classList.remove("error");
     email.placeholder = defaultPlaceholders.email;
-    return true;
-  }
+  }, 3000);
 }
 
-function messageValidation() {
-  if (!pattern.message.test(message.value)) {
-    message.classList.add("error");
-    message.placeholder = errorMessages.message;
-    message.value = "";
-    setTimeout(() => {
-      message.classList.remove("error");
-      message.placeholder = defaultPlaceholders.message;
-    }, 3000);
-    return false;
-  } else {
+function isMessageValid() {
+  return pattern.message.test(message.value);
+}
+
+function showMessageError() {
+  message.classList.add("error");
+  message.placeholder = errorMessages.message;
+  message.value = "";
+
+  setTimeout(() => {
     message.classList.remove("error");
     message.placeholder = defaultPlaceholders.message;
-    return true;
-  }
+  }, 3000);
 }
 
-function inputValidation() {
-  const isNameValid = nameValidation();
-  const isEmailValid = emailValidation();
-  const isMessageValid = messageValidation();
-  return isNameValid && isEmailValid && isMessageValid && checkbox.checked;
+function isCheckboxChecked() {
+  return checkbox.checked;
 }
+
+function showCheckboxError() {
+  errorMessage.style.color = "#b86363";
+
+  setTimeout(function () {
+    errorMessage.style.color = "transparent";
+  }, 3000);
+}
+
+function validateForm() {
+  let valid = true;
+
+  if (!isNameValid()) {
+    showNameError();
+    valid = false;
+  }
+
+  if (!isEmailValid()) {
+    showEmailError();
+    valid = false;
+  }
+
+  if (!isMessageValid()) {
+    showMessageError();
+    valid = false;
+  }
+
+  // if (!isCheckboxChecked()) {
+  //   showCheckboxError();
+  //   valid = false;
+  // }
+
+  return valid;
+}
+
+addEventListener("change", () => {
+  submitBtn.disabled = !submitBtn.disabled;
+});
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  if (!inputValidation()) {
-    errorMessage.style.color = "#b86363";
-    setTimeout(() => {
-      errorMessage.style.color = "transparent";
-    }, 3000);
-
-    return;
-  }
+  if (!validateForm()) return;
   console.log("Mail gesendet");
   form.reset();
   // emailjs.sendForm("service_urv966s", "template_6ro37zj", form).then(
